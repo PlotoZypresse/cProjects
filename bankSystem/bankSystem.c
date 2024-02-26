1486def7d371968ae9125b14ff08b8d1efc8e1ee
PlotoZypresse <janlucca1401@gmail.com>
2024-02-26 17:39:34 +0100

    acc creation added

--- bankSystem/bankSystem.c
+++ bankSystem/bankSystem.c
@@ -1,5 +1,40 @@
-int int main(int argc, char *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct acc{
+  float accBalance;
+  //int accNumber; 
+  char* accName;
+};
+
+struct acc* createAcc(char* name){
+  // create acc with name
+  struct acc* newAcc = (struct acc*)malloc(sizeof(struct acc));
+  
+  if(newAcc != NULL){
+    newAcc->accName = strdup(name); 
+    newAcc->accBalance = 0.0;
+    
+    return newAcc;
+  }
+  
+  return 0;
+}
+
+int main(int argc, char *argv[])
 {
+  struct acc* myAcc = createAcc("Peter Griffin");
+  if(myAcc != NULL){
+    printf("account crated for %s\n", myAcc->accName);
+
+    free(myAcc->accName);
+    free(myAcc);
+  }
+  else{
+    printf("Account creation failed");
+  }
 
   return 0;
 }
+
