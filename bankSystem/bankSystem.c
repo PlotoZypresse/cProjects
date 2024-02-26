0fc1e0c6c00a6730d8f73fec2e7aa0cc199b88f6
PlotoZypresse <janlucca1401@gmail.com>
2024-02-26 18:22:32 +0100

    deposits

--- bankSystem/bankSystem.c
+++ bankSystem/bankSystem.c
@@ -8,7 +8,11 @@ struct acc{
   char* accName;
 };
 
-struct acc* createAcc(char* name){
+//int accNumbers[] = {};
+
+// creating new account
+struct acc* createAcc(char* name)
+{
   // create acc with name
   struct acc* newAcc = (struct acc*)malloc(sizeof(struct acc));
   
@@ -16,24 +20,40 @@ struct acc* createAcc(char* name){
     newAcc->accName = strdup(name); 
     newAcc->accBalance = 0.0;
     
+    printf("New account with name %s has been created\n", newAcc->accName);
+    printf("Account Balance %.2f\n", newAcc->accBalance);
+
     return newAcc;
   }
-  
   return 0;
+}
+
+// deposit into existing account
+void deposit(float amount, struct acc* account)
+{
+  if(account != NULL && amount > 0){
+    account->accBalance += amount;
+    printf("%.2f have been deposited into your account(%s). Your new balance is now %.2f\n"
+           , amount, account->accName, account->accBalance);
+  }
+  else {
+    printf("Deposti failed\n");  
+  }
 }
 
 int main(int argc, char *argv[])
 {
   struct acc* myAcc = createAcc("Peter Griffin");
-  if(myAcc != NULL){
-    printf("account crated for %s\n", myAcc->accName);
+  if(myAcc == NULL){
+     printf("Something went wrong. No account was created");
+  }
 
-    free(myAcc->accName);
-    free(myAcc);
-  }
-  else{
-    printf("Account creation failed");
-  }
+  deposit(69.69, myAcc);
+
+
+  free(myAcc->accName);
+  free(myAcc);
+
 
   return 0;
 }
