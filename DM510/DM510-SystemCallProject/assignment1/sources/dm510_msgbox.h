c20aecf0a8b924cd0186007931a75e289d2e6e12
PlotoZypresse <janlucca1401@gmail.com>
2024-04-05 15:29:51 +0200

    structure changes

--- DM510/DM510-SystemCallProject/assignment1/sources/dm510_msgbox.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DM510_MSGBOX_H
-#define __DM510_MSGBOX_H
-
-extern int dm510_msgbox_put( char*, int );
-extern int dm510_msgbox_get( char*, int );
-
-#endif //JATHU21_DM510_MSGBOX_H
--- DM510/DM510-SystemCallProject/assignment1/sources/dm510_msgbox_put.c
+++
@@ -1,8 +0,0 @@
-#include "linux/kernel.h"
-#include "linux/unistd.h"
-#include "/home/jathu21/dm510/linux-6.6.9/arch/um/include/asm/dm510_msgbox.h"
-
-asmlinkage
-int sys_dm510_msgbox_put(char* buffer, int length) {
-    return dm510_msgbox_put(buffer, length);
-}
